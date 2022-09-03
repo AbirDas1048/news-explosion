b1d879bc222e2a709f6825f0b715c9827c070ff3
Abir Das <abirdas422@gmail.com>
2022-09-03 20:04:30 +0600

    Loader js

--- js/main.js
+++ js/main.js
@@ -45,9 +45,12 @@ const displayCategories = data => {
     categories.forEach(category => {
         const link = document.createElement('a');
         link.classList.add('nav-link');
+        //link.classList.add('active');
         link.innerText = category.category_name;
 
-        link.setAttribute('id', `${category.category_id}`)
+        link.setAttribute('onclick', `loadNews('${category.category_id}', '${category.category_name}')`);
+
+        link.setAttribute('id', `${category.category_id}`);
         //console.log(category);
         categoryNav.appendChild(link);
     });
@@ -55,25 +58,42 @@ const displayCategories = data => {
 }
 
 
-const loadNews = () => {
-    fetch('https://openapi.programming-hero.com/api/news/category/08')
+const loadNews = (category_id, category_name) => {
+    toogleLoader(true);
+    const allLinks = document.querySelectorAll('#category-nav a');
+    allLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+    //allLinks.classList.remove('active');
+    const selectedLink = document.getElementById(category_id);
+    selectedLink.classList.add('active');
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    //console.log(category_id)
+    fetch(url)
         .then(response => response.json())
-        .then(data => {
-            displayNews(data);
-        })
+        .then(data => displayNews(data, category_name))
         .catch(error => {
             console.error(error);
         });
 }
 
-loadNews();
+//loadNews();
 
-const displayNews = data => {
+const displayNews = (data, category_name) => {
 
     const newsSection = document.getElementById('news-section');
-
+    newsSection.textContent = "";
     const allNews = (data.data);
-    //console.log(allNews);
+    const newsLength = allNews.length;
+    const countNews = document.getElementById('count-news');
+    if (newsLength > 0) {
+        countNews.innerText = `${newsLength} items found for category ${category_name}`;
+    } else {
+        countNews.innerText = `No items found for category ${category_name}`;
+    }
+    //console.log(newsLength);
+
+    allNews.sort((a, b) => b.total_view - a.total_view);
 
     allNews.forEach(news => {
         const div = document.createElement('div');
@@ -81,10 +101,9 @@ const displayNews = data => {
         div.classList.add('mb-3');
         div.classList.add('border-0');
         let details = news.details;
-        //console.log(news.author.rating);
 
-        if (details.length >= 754) {
-            details = details.slice(0, 754) + '...';
+        if (details.length >= 600) {
+            details = details.slice(0, 600) + '...';
         } else {
             details = details
         }
@@ -95,29 +114,29 @@ const displayNews = data => {
                     </div>
                     <div class="col-md-9 col-12">
                         <div class="card-body">
-                            <h5 class="card-title">${news.title}</h5>
-                            <p class="card-text">${details}</p>
-                            <div class="row">
-                                <div class="col-3">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <img src="${news.author.img}" class="img-fluid rounded-circle" alt="...">
+                            <h3 class="card-title fw-bolder">${news.title}</h3>
+                            <p class="card-text text-black-50">${details}</p>
+                            <div class="row align-items-center justify-content-between g-3 mt-2 fw-semibold">
+                                <div class="col-lg-6 col-md-6 col-12">
+                                    <div class="row align-items-center">
+                                        <div class="col-lg-2 col-md-4 col-2">
+                                            <img src="${news.author.img}" class="img-fluid rounded-circle w-100" alt="...">
                                         </div>
-                                        <div class="col-8">
-                                            <p>${news.author.name}</p>
-                                            <p>${news.author.published_date}</p>
+                                        <div class="col-lg-10 col-md-8 col-10">
+                                            <p>${news.author.name ? news.author.name : 'No data found'}</p>
+                                            <p class="text-black-50">${news.author.published_date ? news.author.published_date : 'No data found'}</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-3">
-                                    <p>${news.total_view}</p>
+                                <div class="col-lg-2 col-md-6 col-4 text-center">
+                                    <p class="text-black-50 fw-bolder"><i class="far fa-eye"></i> ${news.total_view ? news.total_view : 'No data found'}</p>
                                 </div>
-                                <div class="col-3">
-                                    <p>${news.rating.badge}</p>
-                                    <p>${news.rating.number}</p>
+                                <div class="col-lg-2 col-md-6 col-4 text-center">
+                                    <p class="text-black-50"><i class="fas fa-star-half-alt"></i> ${news.rating.number ? news.rating.number : 'No data found'}</p>
                                 </div>
-                                <div class="col-3">
-                                    <p></p>
+                                <div class="col-lg-2 col-md-6 col-4 text-center">
+                                    
+                                    <button type="button" onclick="newsDetails('${news._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsModal">Details</button>
                                 </div>
                             </div>
                         </div>
@@ -126,10 +145,20 @@ const displayNews = data => {
 
         //link.setAttribute('id', `${category.category_id}`)
         //console.log(category);
+
+        // <p class="text-primary"><i class="fas fa-arrow-right"></i></p>
         newsSection.appendChild(div);
     });
 
+    toogleLoader(false);
+
 }
 
-
-
+const toogleLoader = (isLoading) => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    } else {
+        loaderSection.classList.add('d-none');
+    }
+}
